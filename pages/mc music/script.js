c67096d5d8df55fdf790404fb5f1f94e52db5a1c
Mqrshie <120640441+Mqrshie@users.noreply.github.com>
2023-03-25 12:33:08 +0800

    i hate coding

---
+++ pages/mc music/script.js
@@ -0,0 +1,22 @@
+const musicPlayer = document.querySelector('#music-player');
+const discs = document.querySelectorAll('.disc');
+let currentAudio = null;
+
+discs.forEach(disc => {
+  disc.addEventListener('click', () => {
+    stopAllAudio();
+    const discId = disc.getAttribute('id').substring(5);
+    const audio = new Audio(`music/${discId}.ogg`);
+    audio.play();
+    currentAudio = audio;
+  });
+});
+
+function stopAllAudio() {
+  const audios = document.querySelectorAll('audio');
+  audios.forEach(audio => audio.pause());
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio = null;
+  }
+}
